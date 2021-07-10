937930562fd860ceeaf254af0ae1f78a2c9e5ce7
elmar8287 <L-mar@inbox.ru>
2021-07-10 16:19:54 +0400

    Fix linter issues

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ function openNav() {
   document.getElementById('myNav').style.width = '100%';
 }
 
+openNav();
+
 function closeNav() {
   document.getElementById('myNav').style.width = '0%';
 }
+
+closeNav();
